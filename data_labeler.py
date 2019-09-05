9540b6449e8b5c90438dd60b94c9688cb2c485e1
Francesco Melchiori <franzmelchiori@gmail.com>
2019-09-05 17:40:23 +0200

    add data_labeler

--- data_labeler.py
+++ data_labeler.py
@@ -21,8 +21,40 @@
 """
 
 
+import numpy as np
+import pandas as pd
+
+import data_viewer
+import data_sampler
+
+
 def main():
-    pass
+    timestamp_start = '2019-02-04 00:00:00'
+    time_zone = 'Europe/Rome'
+    sampling_period = '1S'
+    series_amount = 10
+    sampling_amount = 100
+    anomaly_start = int(sampling_amount/2)
+    anomaly_amount = int(sampling_amount/4)
+    anomaly_pulse = np.zeros(sampling_amount)
+    anomaly_pulse[anomaly_start:anomaly_start+anomaly_amount] += 1
+
+    pd_series_dictionary_test = {}
+    series_counter = range(1, series_amount+1)
+    for series_number in series_counter:
+        data_test = np.random.normal(0, 1, sampling_amount)
+        data_test[:] += anomaly_pulse * 10
+        timezone_index_test = pd.date_range(timestamp_start,
+                                            periods=sampling_amount,
+                                            freq=sampling_period,
+                                            tz=time_zone)
+        utc_index_test = pd.to_datetime(timezone_index_test, utc=True)
+        pd_series_test = pd.Series(data_test, index=utc_index_test)
+        pd_series_dictionary_name = 'pd_series_test_' + str(series_number)
+        pd_series_dictionary_test[pd_series_dictionary_name] = pd_series_test
+    pd_dataframe_test = pd.DataFrame(pd_series_dictionary_test)
+
+    data_viewer.view_pd_dataframe(pd_dataframe_test)
 
 
 if __name__ == '__main__':
