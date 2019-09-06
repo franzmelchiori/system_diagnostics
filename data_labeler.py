a92c773f967fd20d3d3f0557c042bcf34eba2718
Francesco Melchiori <franzmelchiori@gmail.com>
2019-09-06 10:41:40 +0200

    update data_labeler

--- data_labeler.py
+++ data_labeler.py
@@ -23,6 +23,8 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 import data_viewer
 import data_sampler
@@ -32,10 +34,13 @@ def main():
     timestamp_start = '2019-02-04 00:00:00'
     time_zone = 'Europe/Rome'
     sampling_period = '1S'
+    event_minimum_period = '1M'
     series_amount = 10
-    sampling_amount = 100
+    sampling_amount = 600
+
     anomaly_start = int(sampling_amount/2)
     anomaly_amount = int(sampling_amount/4)
+    anomaly_amplitude = 10
     anomaly_pulse = np.zeros(sampling_amount)
     anomaly_pulse[anomaly_start:anomaly_start+anomaly_amount] += 1
 
@@ -43,7 +48,7 @@ def main():
     series_counter = range(1, series_amount+1)
     for series_number in series_counter:
         data_test = np.random.normal(0, 1, sampling_amount)
-        data_test[:] += anomaly_pulse * 10
+        data_test[:] += anomaly_pulse * anomaly_amplitude
         timezone_index_test = pd.date_range(timestamp_start,
                                             periods=sampling_amount,
                                             freq=sampling_period,
@@ -53,8 +58,20 @@ def main():
         pd_series_dictionary_name = 'pd_series_test_' + str(series_number)
         pd_series_dictionary_test[pd_series_dictionary_name] = pd_series_test
     pd_dataframe_test = pd.DataFrame(pd_series_dictionary_test)
-
     data_viewer.view_pd_dataframe(pd_dataframe_test)
+
+    pd_dataevent_samples, pd_dataevent_sample_length = \
+        data_sampler.sample_dataevents(pd_dataframe_test,
+                                       event_minimum_period)
+    # pd_dataevent_anomaly_sample_start = int(pd_dataevent_samples.__len__()/2)
+    # data_viewer.view_pd_dataframe(
+    #     pd_dataevent_samples[pd_dataevent_anomaly_sample_start])
+
+    pd_dataevent_transposed_samples, pd_dataevent_sample_timestamps = \
+        data_sampler.transpose_dataevents(pd_dataevent_samples)
+    # plt.plot(
+    #     pd_dataevent_transposed_samples[pd_dataevent_anomaly_sample_start])
+    # plt.show()
 
 
 if __name__ == '__main__':
